1d73d76daf0f43e08e10102e9e07d65ab5d7e49b
vidklopcic <klopcic.vid@gmail.com>
2020-12-22 10:34:01 +0100

    add width and height to context

--- lib/extended_functionality/context.dart
+++ lib/extended_functionality/context.dart
@@ -9,6 +9,14 @@ extension ContextExtensions on BuildContext {
     return MediaQuery.of(this).size.shortestSide * fraction;
   }
 
+  double get width {
+    return MediaQuery.of(this).size.width;
+  }
+
+  double get height {
+    return MediaQuery.of(this).size.height;
+  }
+
   ThemeData get theme => Theme.of(this);
   TextTheme get textTheme => Theme.of(this).textTheme;
 }
