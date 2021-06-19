f541e6637562f1acc408504887c1f0f773ec94ff
vidklopcic <klopcic.vid@gmail.com>
2021-06-19 12:00:57 +0200

    add callAtStart option to throttle helper

--- lib/utils/event_utils.dart
+++ lib/utils/event_utils.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 class EventUtils {
   Map debounceTimeouts = {};
+  Map throttleMethods = {};
 
   void debounce(int timeoutMs, Function target, {key, List arguments}) {
     key = key ?? target;
@@ -19,11 +20,16 @@ class EventUtils {
   void throttle(int period, Function target, {key, List arguments, bool callAtStart = false}) {
     key = key ?? target;
     if (debounceTimeouts[key]?.isActive ?? false) {
+      throttleMethods[key] = target;
       return;
     }
 
     Timer timer = Timer(Duration(milliseconds: period), () {
-      if (!callAtStart) Function.apply(target, arguments ?? []);
+      if (!callAtStart)
+        Function.apply(target, arguments ?? []);
+      else if (throttleMethods[key] != null) {
+        Function.apply(throttleMethods[key], arguments ?? []);
+      }
     });
 
     if (callAtStart) Function.apply(target, arguments ?? []);
