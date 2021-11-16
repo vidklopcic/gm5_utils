745852f918add2441994419f2c76f01445fc110f
vidklopcic <klopcic.vid@gmail.com>
2021-11-16 14:52:33 +0100

    OverflowableStack

---
+++ lib/components/overflowable_stack.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class OverflowableStack extends Stack {
+  OverflowableStack({
+    Key key,
+    AlignmentGeometry alignment = AlignmentDirectional.topStart,
+    TextDirection textDirection,
+    StackFit fit = StackFit.loose,
+    Clip clipBehavior = Clip.none,
+    List<Widget> children = const <Widget>[],
+  }) : super(
+          key: key,
+          alignment: alignment,
+          textDirection: textDirection,
+          fit: fit,
+          clipBehavior: clipBehavior,
+          children: children,
+        );
+
+  @override
+  RenderStack createRenderObject(BuildContext context) {
+    return RenderStack2(
+      alignment: alignment,
+      textDirection: textDirection ?? Directionality.of(context),
+      fit: fit,
+      clipBehavior: clipBehavior,
+    );
+  }
+}
+
+class RenderStack2 extends RenderStack {
+  RenderStack2({
+    List<RenderBox> children,
+    AlignmentGeometry alignment = AlignmentDirectional.topStart,
+    TextDirection textDirection,
+    StackFit fit = StackFit.loose,
+    Clip clipBehavior = Clip.none,
+  }) : super(
+          children: children,
+          alignment: alignment,
+          textDirection: textDirection,
+          fit: fit,
+          clipBehavior: clipBehavior,
+        );
+
+  @override
+  bool hitTest(BoxHitTestResult result, {Offset position}) {
+    if (hitTestChildren(result, position: position) || hitTestSelf(position)) {
+      result.add(BoxHitTestEntry(this, position));
+      return true;
+    }
+    return false;
+  }
+}
