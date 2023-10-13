dfe35f78916c6160ba96b594ea3f60f7d0fde49b
vidklopcic <klopcic.vid@gmail.com>
2023-10-13 13:29:57 +0200

    added Future callback

--- lib/extended_functionality/async.dart
+++ lib/extended_functionality/async.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 
-extension FutureExtensions on Future {
+extension FutureExtensions<T> on Future<T> {
   bool get isCompleted {
     bool completed = false;
     this.whenComplete(() => completed = true);
     return completed;
+  }
+
+  Future<T> fallback(T fallbackValue) {
+    return this.then((value) {
+      if (value.runtimeType == List) {
+        if ((value as List).isEmpty) {
+          return fallbackValue;
+        }
+      }
+      return value ?? fallbackValue;
+    });
   }
 }
